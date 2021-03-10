dae96b8b1d4750289b0a68b9f06349833a6251c6
Babak Bigdeli <babakbigdeli@gmail.com>
2021-03-10 16:23:47 +0000

    creating schema elements

--- models/workout.js
+++ models/workout.js
@@ -2,5 +2,48 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+//creating schema elements
+const workoutSchema = new Schema ({
+    day: {
+        type: Date,
+        default: Date.now
+    },
 
+    exercices: [{
+
+        type: {
+            type: String,
+            required: "Type is required"
+        },
+
+        name: {
+            type: String,
+            required: "Name is required"
+        },
+
+        duration: {
+            type: Number,
+            required: "Druration is required"
+        },
+
+        weight: {
+            type: Number,
+        },
+
+        reps: {
+            type: Number,
+        },
+
+        sets: {
+            type: Number,
+        },
+
+        distance: {
+            type: Number,
+        }
+
+    }]
+
+
+});
 
