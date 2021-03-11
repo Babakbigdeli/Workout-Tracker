b31d6c22b08e1624ed6098392198874ccdc7a9ef
Babak Bigdeli <babakbigdeli@gmail.com>
2021-03-11 16:35:55 +0000

    get all route

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -2,3 +2,14 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+//a get request to query all first
+
+router.get("api/workouts", (req, res) => {
+    Workout.find()
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
